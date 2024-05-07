b2eb45d48d162c782649dbffb15ce61954736826
charlesobird <157501716+charlesobird@users.noreply.github.com>
2024-05-07 10:04:26 +0100

    add new page

---
+++ app/src/main/java/com/project/starwarsquiz/CategorySelection.kt
@@ -0,0 +1,11 @@
+package com.project.starwarsquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategorySelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.category_selection)
+    }
+}
