8a6ff7c0e0093ad0422b00dcb9da7dd991944f9c
charlesobird <157501716+charlesobird@users.noreply.github.com>
2024-05-07 12:35:58 +0100

    fix font issue

--- app/src/main/java/com/project/starwarsquiz/CategorySelection.kt
+++ app/src/main/java/com/project/starwarsquiz/CategorySelection.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class CategorySelection : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.category_selection)
+        setContentView(R.layout.activity_category_selection)
     }
 }
